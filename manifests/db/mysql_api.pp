27059b38f70dae824140c34485b74b82319d8412
Andrew Woodward <awoodward@mirantis.com>
2016-04-04 14:53:37 -0700

    [9-Kilo] Stub nova::db::mysql_api
    Newer manifests have it, so we will stub it for now

---
+++ manifests/db/mysql_api.pp
@@ -0,0 +1,9 @@
+class nova::db::mysql_api (
+  $user           = undef,
+  $password       = undef,
+  $dbname         = undef,
+  $allowed_hosts  = undef,
+  )
+{
+  notify('nova::db::mysql_api stub')
+}
